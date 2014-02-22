73241b2b6f153669a43090e50e8fc6d27793bf8b
Rames Aliyev <creator@ramesaliyev.com>
2014-02-22 17:31:21 +0200

    Prism & Css.Light & Hbs.Post
    - post.hbs "Etiketleme" "Etiketler" olarak düzenlendi.
    - PrismJS için yardımcı JavaScript yazıldı. (Bknz:
    http://ramesaliyev.com/ghosta-syntax-highlighting-eklemek/)
    - Css içerisine .light class'ı eklendi

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,6 +13,35 @@
 
         //$("#blog-logo").prependTo("#site-head-content");
 
+        $("code.ghost-prism").each(function(index, element){
+            // Elementlerimizi ve verilerimizi toplayalım.
+            var $code     = $(element),
+                $pre      = $code.parent("pre"),
+                data      = $code.data(),
+                html      = $code.html().trim(),
+                classList = "";
+
+            // Prism Lang belittiysek class'ımızı oluşturalım ve ekleyelim.
+            if(data.prismLang) classList += " language-"+data.prismLang;
+
+            // Prism Line-Numbers belirttiysek class'ımızı ekleyelim.
+            if(data.prismLinenumbers) classList += " line-numbers";
+
+            // Prism Line Higlight belirttiysek direkt olark attribute'muzu atayalım.
+            if(data.prismLinehighlight) $pre.attr("data-line", data.prismLinehighlight);
+
+            // Class listemizi ekleyelim.
+            $pre.addClass(classList);
+            // Markdown formatı sonlarda gereksiz yeni satırlar yaratabiliyor, bu nedenle trim edilmiş yeni html'imizi aktaralım.
+            $code.html(html);
+            // Çift çağrılara karşı "ghost-prism" class'ımızı silelim.
+            $code.removeClass("ghost-prism");
+
+            // Prism API'ımızı tetikleyelim.
+            Prism.highlightAll();
+
+        });
+
     });
 
 }(jQuery));
